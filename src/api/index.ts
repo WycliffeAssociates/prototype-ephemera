20c24afaf01b012ca3b28988ad64c3c56b99b857
Darrell Cole Hill <darrellcolehill@gmail.com>
2022-08-29 10:00:57 -0400

    removed test dummy test data folders

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,36 +1,9 @@
 import { cp } from "fs";
-
-let PHMXML = require('./testData/PHM.xml');
-let PHMTxt = require('./testData/PHMJSON.txt');
 let PHMJSON = require('./testData/PHM.json');
-
-// let XMLParser = require('react-xml-parser');
-
-// function getBook(book : number)
-// {
-
-//     return PHMJSON.book[book];
-// }
-
-// function getChapter(chapter : number)
-// {
-
-//     return PHMJSON;
-// }
-
-// function getVerse(verse : number)
-// {
-
-//     return PHMJSON;
-// }
 
 function getChapter()
 {
-    // console.log(PHMJSON.xml.book.chapter.verse);
     return PHMJSON.xml.book.chapter.verse;
 }
 
 export default getChapter;
-
-
-
