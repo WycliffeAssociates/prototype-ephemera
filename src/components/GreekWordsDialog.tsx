28a01bd722cc6cbc4d02a95a6e2e1206f6f633a9
Darrell Hill <darrell_hill@wycliffeassociates.org>
2022-09-23 10:49:52 -0400

    refactored

--- src/components/GreekWordsDialog.tsx
+++ src/components/GreekWordsDialog.tsx
@@ -3,36 +3,25 @@ import Box from '@mui/material/Box';
 import GreekWordInfo from './GreekWordInfo';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import IconButton from '@mui/material/IconButton';
-import { useEffect } from 'react';
-import { GreekPhraseWord } from '../types'
+import { GreekPhraseWord, FormattedGreekWord } from '../types'
 
-
-let word1 : GreekPhraseWord = {
+let testWord1 : GreekPhraseWord = {
   greekWords: "greek word here/more words",
   englishWords: "hello",
   morphology: "some morphology here",
   descriptions: ["description1, description2","description3"]
 }
 
+let testData = [testWord1, testWord1, testWord1] as GreekPhraseWord[];
 
-let testData2 = [word1, word1, word1] as GreekPhraseWord[];
 
 interface GreekWordsDialogProps {
   open: Boolean;
   onClose: () => void;
-  greekWord: any;
+  greekWords: FormattedGreekWord[];
 };
 
-function GreekWordsDialog({open, onClose, greekWord} : GreekWordsDialogProps) {
-
-
-    useEffect(() => {
-      if(greekWord.length > 0)
-      {
-        console.log(greekWord);
-      }
-    }, [greekWord])
-
+function GreekWordsDialog({open, onClose, greekWords} : GreekWordsDialogProps) {
 
     return ( 
       <Box 
@@ -64,8 +53,8 @@ function GreekWordsDialog({open, onClose, greekWord} : GreekWordsDialogProps) {
 
             <Grid item lg={11} xl={11} md={11} style={{maxHeight: '89vh', overflow: 'auto'}}>
 
-              {testData2.map((data, idx) => (
-                <GreekWordInfo key={idx} {...data}/>
+              {greekWords.map((data, idx) => (
+                <GreekWordInfo key={idx} currentGreekWord={data}/>
               ))}
               
             </Grid>
