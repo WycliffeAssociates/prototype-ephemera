eaa9c3c564754798625c5761f75c766b380ac61d
Darrell Cole Hill <darrellcolehill@gmail.com>
2022-09-26 08:50:33 -0400

    updated styles

--- src/components/NextChapterButton.tsx
+++ src/components/NextChapterButton.tsx
@@ -6,16 +6,7 @@ function NextChapterButton()
 {
   return (
     <Box display={{ xs: 'block', sm: 'block', md:"none", lg:"none", xl:"none" }}>
-      <Fab color="primary" aria-label="add" style={{
-          width: "50px",
-          height: "50px",
-          backgroundColor:"white", 
-          color:"black",
-          position: "fixed",
-          top: "85%",
-          right: "5%"
-        }}
-        onClick={() => {console.log("onclickhere")}}>
+      <Fab className="TextViewContainer__NextChapterButton" color="primary" aria-label="add" onClick={() => {console.log("onclickhere")}}>
         <p>N</p>
       </Fab>
     </Box>
