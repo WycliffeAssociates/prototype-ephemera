9899a0730211f757e17d27a9fc55a8c805ef9100
Darrell Cole Hill <darrellcolehill@gmail.com>
2022-10-05 09:26:44 -0400

    fixed merge error with mobile chapter navi buttons

--- src/components/NextChapterButton.tsx
+++ src/components/NextChapterButton.tsx
@@ -6,8 +6,18 @@ function NextChapterButton()
 {
   return (
     <Box display={{ xs: 'block', sm: 'block', md:"none", lg:"none", xl:"none" }}>
-      <Fab className="TextViewContainer__NextChapterButton" color="primary" aria-label="add" onClick={() => {console.log("onclickhere")}}>
-        <p>N</p>
+      <Fab color="primary" aria-label="add"
+        style={{
+            width: "50px",
+            height: "50px",
+            backgroundColor:"white",
+            color:"black",
+            position: "fixed",
+            top: "85%",
+            right: "5%"
+          }}
+      >
+        <p>I</p>
       </Fab>
     </Box>
   )
