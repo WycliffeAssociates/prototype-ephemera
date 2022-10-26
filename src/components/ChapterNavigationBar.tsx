355dae7b173bb64f54990f874ca2b74b224befde
Darrell Cole Hill <darrellcolehill@gmail.com>
2022-10-26 11:29:35 -0400

    added onclick and correct book/chapter display

--- src/components/ChapterNavigationBar.tsx
+++ src/components/ChapterNavigationBar.tsx
@@ -2,24 +2,32 @@ import Grid from '@mui/material/Grid';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
 import SettingsIcon from '@material-ui/icons/Settings';
+import useBookChapterParams from '../hooks/useBookChapterParams';
 
 
+interface ChapterNavigationBarProps {
+  onClick: (params: any) => any
+}
 
-function ChapterNavigationBar()
+function ChapterNavigationBar( {onClick} : ChapterNavigationBarProps)
 {
+
+  let bookChapter = useBookChapterParams();
+
+
   return (
 
     <Grid container direction="row" justifyContent={{lg: "center", md: "center", sm:"flex-start"}} alignItems="flex-start" style={{marginLeft:"0px", marginBottom:"0px", height:"100%"}}>
       <Grid item lg={6} md={6} sm={10} xs={10}>
         <ButtonGroup variant="outlined" aria-label="outlined button group" style={{width:"100%", height:"48px", backgroundColor:"#f2f2f2", borderRadius:"25px 25px 25px 25px"}} >
-          <Button onClick={() => {console.log("onClickHere")}} style={{width:"85%", borderColor:"#d9d9d9", borderRadius:"25px 0px 0px 25px"}}> 
+          <Button onClick={onClick} style={{width:"85%", borderColor:"#d9d9d9", borderRadius:"25px 0px 0px 25px"}}> 
             <span className="ChapterNavigationBar__Span ChapterNavigationBar__BookName">
-                Book
-              </span>
+              {bookChapter.book}
+            </span>
           </Button>
-          <Button onClick={() => {console.log("onClickHere")}} style={{width:"15%", borderColor:"#d9d9d9", borderRadius:"0px 25px 25px 0px"}}>
+          <Button onClick={onClick} style={{width:"15%", borderColor:"#d9d9d9", borderRadius:"0px 25px 25px 0px"}}>
             <span className="ChapterNavigationBar__Span">
-              Ch
+              {bookChapter.chapter}
             </span>
           </Button>
         </ButtonGroup>
