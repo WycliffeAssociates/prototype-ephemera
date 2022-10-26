1397d29a4872043bba28d00df0ec1b115b07747d
Darrell Cole Hill <darrellcolehill@gmail.com>
2022-10-26 11:53:26 -0400

    fixed text color and scrolling bug

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -16,8 +16,41 @@ function Text({onPhraseClick}: TextProps)
   const bookChapter = useBookChapterParams();
   const verses = useChapterVerseData(bookChapter.book, bookChapter.chapter);
   const windowSize = useWindowSize([bookChapter.book, bookChapter.chapter]);
-
   const [childClicked, setChildClicked] = useState<any>({});
+
+
+  useEffect(() => {
+    // NOTE: 900px is the current breakpoint for mobile devices. 
+    // Scroll to top when possible, else scroll to the bottom
+    // TODO: add case where phrase is already at the top or at the bottom
+    if(childClicked?.current !== undefined && windowSize.innerWidth < 900)
+    {
+      let startBottom = childClicked.current.getBoundingClientRect().bottom;
+      childClicked?.current.scrollIntoView({block: "start"})
+      let endBottom = childClicked.current.getBoundingClientRect().bottom;
+
+      if(startBottom == endBottom)
+      {
+        childClicked?.current.scrollIntoView({block: "end"})
+      }
+    }
+  }, [childClicked, windowSize.innerWidth])
+
+
+
+  useEffect(() => {
+
+    // Makes sure that all text is default color after navigating to another chapter. 
+    if(childClicked?.current?.style?.color !== undefined) 
+    {
+      childClicked.current.style.color = "#001533CC";
+    } 
+
+    // Makes sure that the next chapter is starting from verse 1. 
+    document.getElementById("TextContainer")?.scroll(0,0)
+
+    setChildClicked({})
+  }, [verses]);
 
 
   function handleChildClicked(newChildClicked: any) {
@@ -35,22 +68,8 @@ function Text({onPhraseClick}: TextProps)
     setChildClicked(newChildClicked);
   }
 
-  useEffect(() => {
-    // NOTE: 900px is the current breakpoint for mobile devices. 
-    // Scroll to top when possible, else scroll to the bottom
-    // TODO: add case where phrase is already at the top or at the bottom
-    if(childClicked?.current !== undefined && windowSize.innerWidth < 900)
-    {
-      let startBottom = childClicked.current.getBoundingClientRect().bottom;
-      childClicked?.current.scrollIntoView({block: "start"})
-      let endBottom = childClicked.current.getBoundingClientRect().bottom;
 
-      if(startBottom == endBottom)
-      {
-        childClicked?.current.scrollIntoView({block: "end"})
-      }
-    }
-}, [childClicked, windowSize.innerWidth])
+
 
 
 
