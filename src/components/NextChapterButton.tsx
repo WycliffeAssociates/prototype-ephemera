6d961caa7b0cee0eeb9f7aed472a666b48b35441
Darrell Cole Hill <darrellcolehill@gmail.com>
2022-11-07 09:24:53 -0500

    added functionality to navigate chapters

--- src/components/NextChapterButton.tsx
+++ src/components/NextChapterButton.tsx
@@ -1,9 +1,37 @@
 import Fab from '@mui/material/Fab';
 import Box from '@mui/material/Box';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import useBookChapterParams from '../hooks/useBookChapterParams';
+import {useNavigate, createSearchParams } from "react-router-dom";
+import { books as newTestamentMetadata} from "../applicationLogic/newTestamentMetadata"
+
 
 
 function NextChapterButton()
 {
+  const navigate = useNavigate();
+  const bookChapter = useBookChapterParams();
+
+  let newChapter = bookChapter.chapter + 1;
+
+  const onClick = () => {
+    if(newChapter > 0 && newChapter <= newTestamentMetadata[bookChapter.book].numChapters) {
+      const params = {
+        book: bookChapter.book,
+        chapter: (newChapter > 0 && newChapter <= newTestamentMetadata[bookChapter.book].numChapters ? newChapter + "" : (bookChapter.chapter - 1) + "")
+      };
+
+      const options = {
+        pathname: '/',
+        search: `?${createSearchParams(params)}`,
+      };
+      navigate(options, { replace: true });
+    }
+    else
+    {
+      console.log("chapter numbe out of bounds");
+    }
+  }
   return (
     <Box display={{ xs: 'block', sm: 'block', md:"none", lg:"none", xl:"none" }}>
       <Fab color="primary" aria-label="add"
@@ -17,7 +45,7 @@ function NextChapterButton()
             right: "5%"
           }}
       >
-        <p>N</p>
+        <ChevronRightIcon onClick={onClick}/>
       </Fab>
     </Box>
   )
--- src/components/PreviousChapterButton.tsx
+++ src/components/PreviousChapterButton.tsx
@@ -1,10 +1,37 @@
 import Fab from '@mui/material/Fab';
 import Box from '@mui/material/Box';
-
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import useBookChapterParams from '../hooks/useBookChapterParams';
+import {useNavigate, createSearchParams } from "react-router-dom";
+import { books as newTestamentMetadata} from "../applicationLogic/newTestamentMetadata"
 
 
 function PreviousChapterButton()
 {
+  const navigate = useNavigate();
+  const bookChapter = useBookChapterParams();
+
+  let newChapter = bookChapter.chapter - 1;
+
+  const onClick = () => {
+    if(newChapter > 0 && newChapter <= newTestamentMetadata[bookChapter.book].numChapters) {
+      const params = {
+        book: bookChapter.book,
+        chapter: (newChapter > 0 && newChapter <= newTestamentMetadata[bookChapter.book].numChapters ? newChapter + "" : (bookChapter.chapter - 1) + "")
+      };
+
+      const options = {
+        pathname: '/',
+        search: `?${createSearchParams(params)}`,
+      };
+      navigate(options, { replace: true });
+    }
+    else
+    {
+      console.log("chapter numbe out of bounds");
+    }
+  }
+
   return (
     <Box display={{ xs: 'block', sm: 'block', md:"none", lg:"none", xl:"none" }}>
       <Fab color="primary" aria-label="add"
@@ -18,7 +45,8 @@ function PreviousChapterButton()
             left: "5%"
           }}
       >
-        <p>P</p>
+        
+        <ChevronLeftIcon onClick={onClick}/>
       </Fab>
     </Box>
   )
