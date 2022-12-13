e35b61253ecaa4f7f56fb4d72113a0f60270f0e6
Darrell Cole Hill <darrellcolehill@gmail.com>
2022-12-13 08:11:16 -0500

    testing icons

--- src/components/ChapterNavigationBar.tsx
+++ src/components/ChapterNavigationBar.tsx
@@ -2,6 +2,8 @@ import Grid from '@mui/material/Grid';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
 import SettingsIcon from '@material-ui/icons/Settings';
+import MenuIcon from '@material-ui/icons/Menu';
+import BookIcon from '@material-ui/icons/Book';
 import useBookChapterParams from '../hooks/useBookChapterParams';
 
 
@@ -21,6 +23,7 @@ function ChapterNavigationBar( {onClick} : ChapterNavigationBarProps)
       <Grid item lg={6} md={6} sm={10} xs={10}>
         <ButtonGroup variant="outlined" aria-label="outlined button group" style={{width:"100%", height:"48px", backgroundColor:"#f2f2f2", borderRadius:"25px 25px 25px 25px"}} >
           <Button onClick={onClick} style={{width:"85%", borderColor:"#d9d9d9", borderRadius:"25px 0px 0px 25px"}}> 
+            <MenuIcon style={{paddingRight:"15px", color:"#001A3D99"}}/>
             <span className="ChapterNavigationBar__Span ChapterNavigationBar__BookName">
               {bookChapter.book}
             </span>
