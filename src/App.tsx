3af52a4db8aff93816974353cdf513062d813d42
Darrell Cole Hill <darrellcolehill@gmail.com>
2023-01-31 09:00:21 -0500

    opens most recent bookChapter or mat 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import {SettingsProvider} from './hooks/SettingsContext';
-import {GreekWordsProvider} from './hooks/GreekWordsContext';
+import { useEffect } from 'react';
+import { useBookChapterParams } from './hooks/useBookChapterParams';
+import { SettingsProvider } from './hooks/SettingsContext';
+import { GreekWordsProvider } from './hooks/GreekWordsContext';
 import { View } from './components/View';
 
 
 function App() {
+  
+  const { setValidBookChapterParams } = useBookChapterParams();
+
+  function navigateToMostRecentBookChapter() {
+    let lastBookChapter : {book: string, chapter: string} | undefined;
+    lastBookChapter = JSON.parse(localStorage.getItem("lastBookChapter") as string);
+
+    if(lastBookChapter) {
+      setValidBookChapterParams(lastBookChapter.book, lastBookChapter.chapter)
+    } else {
+      // defaults to Matthew 1, since it is the first book in NT
+      setValidBookChapterParams("Matthew", "1");
+    }
+  }
+
+  useEffect(() => {
+    navigateToMostRecentBookChapter()
+  }, []);
 
   return (
     <div className="App">
--- src/hooks/useBookChapterParams.tsx
+++ src/hooks/useBookChapterParams.tsx
@@ -4,14 +4,35 @@ import {useNavigate, createSearchParams } from "react-router-dom";
 import { books as newTestamentMetadata } from "../applicationLogic/newTestamentMetadata";
 
 
-export function useBookChapterParams()
-{
+function validateBookChapter(book: string, chapter: string) : boolean {
+  if(newTestamentMetadata[book] !== undefined) {
+    let chapterNumber = parseInt(chapter)
+    if(chapterNumber > 0 && chapter <= newTestamentMetadata[book].numChapters) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function storeValidBookChapterParams(book: string, chapter: string) {
+  if(validateBookChapter(book, chapter)) {
+
+    let previousBookChapter = JSON.parse(localStorage.getItem("lastBookChapter") as string);
+    if(previousBookChapter?.chapter !== chapter) {
+      console.log("storing previousBookChapter as ");
+      console.log(book + " " + chapter);
+      localStorage.setItem("lastBookChapter", JSON.stringify({"book": book, "chapter": chapter}));
+    }
+  }
+}
+
+export function useBookChapterParams() {
     const { search } = useLocation();
     const [book, setBook] = useState("");
     const [chapter, setChapter] = useState(1);
     const navigate = useNavigate()
 
-    function setBookChapterParams(book: string, chapter: string) {
+    function setValidBookChapterParams(book: string, chapter: string) {
       
       if(validateBookChapter(book, chapter)) {
         const params = {
@@ -33,41 +54,37 @@ export function useBookChapterParams()
     }
 
     useEffect(() => {
-      let queryString = search.split("?");
-      queryString = queryString[1].split("&");
-  
-      queryString.forEach((param) => {
-        let paramArr = param.split("=");
-        let paramName = paramArr[0];
-        let paramValue = paramArr[1];
-  
-        if(paramName === "book")
-        {
-          let book = paramValue.replace("%20", " ");
-          book = paramValue.replace("+", " ")
-          setBook(book);
-        }
-  
-        if(paramName === "chapter")
-        {
-          setChapter(parseInt(paramValue));
-        }
-      })
+      if(search.length !== 0 && search !== undefined) {
+
+        let queryString = search.split("?");
+        queryString = queryString[1].split("&");
+    
+        queryString.forEach((param) => {
+          let paramArr = param.split("=");
+          let paramName = paramArr[0];
+          let paramValue = paramArr[1];
+    
+          if(paramName === "book")
+          {
+            let book = paramValue.replace("%20", " ");
+            book = paramValue.replace("+", " ")
+            setBook(book);
+          }
+    
+          if(paramName === "chapter")
+          {
+            setChapter(parseInt(paramValue));
+          }
+        });
+      }
     }, [search]);
 
-    return {"setBookChapterParams": setBookChapterParams, "getBookChaptersParams": getBookChaptersParams};
+    useEffect(() => {
+      storeValidBookChapterParams(book, chapter + "");
+    }, [book, chapter]);
+
+    return {"setValidBookChapterParams": setValidBookChapterParams, "getBookChaptersParams": getBookChaptersParams};
 }
 
-function validateBookChapter(book: string, chapter: string) : boolean {
-  // TODO: add validity check here
-  // make sure is valud book AND make sure is valid chapter
-  if(newTestamentMetadata[book] !== undefined) {
-    let chapterNumber = parseInt(chapter)
-    if(chapterNumber > 0 && chapter <= newTestamentMetadata[book].numChapters) {
-      return true;
-    }
-  }
-  return false;
-}
 
 export default useBookChapterParams;
