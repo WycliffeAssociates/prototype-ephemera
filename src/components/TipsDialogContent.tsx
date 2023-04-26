5aac474f32ae12da7ead89e028ec2727a6e3c537
Darrell Hill <darrell_hill@wycliffeassociates.org>
2023-04-25 20:36:26 -0400

    Fixed typo

--- src/components/TipsDialogContent.tsx
+++ src/components/TipsDialogContent.tsx
@@ -26,7 +26,7 @@ export function TipsDialogContent({open, onClose} : TipsDialogContentProps) {
             <p>To view a Greek translation, select the desired word or phrase.</p>
             <p>If you'd like to view a new word, select the desired word to view a new translation. Once a word is selected it will be underlined in blue.</p>
             <h4>Navigation</h4>
-            <p>To change a boor or chapter click the book name or chapter number located on teh left panel above the Scripture text.</p>
+            <p>To change a book or chapter click the book name or chapter number located on teh left panel above the Scripture text.</p>
             <h4>Settings</h4>
             <p>To change how you view the font size, click the settings button at the top of the page.</p>
         </Grid>
