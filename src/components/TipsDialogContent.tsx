378ec86db16b4671a4035e435c10f7e07adba2dc
Darrell Cole Hill <darrellcolehill@gmail.com>
2023-05-19 17:20:26 -0400

    Added settings to Tips dialog

--- src/components/TipsDialogContent.tsx
+++ src/components/TipsDialogContent.tsx
@@ -1,5 +1,7 @@
 import Grid from '@mui/material/Grid';
 import InfoIcon from '@material-ui/icons/InfoOutlined';
+import { useSettings } from '../hooks/SettingsContext';
+import { mapValidGWTSettings } from './GreekWordInfo/utils/mapValidGWTSettings';
 
 interface TipsDialogContentProps {
     open: Boolean;
@@ -8,10 +10,15 @@ interface TipsDialogContentProps {
   
 
 export function TipsDialogContent({open, onClose} : TipsDialogContentProps) {
+
+  const { GWTSettings } = useSettings();
+  let overwriteStyle : any = mapValidGWTSettings(GWTSettings);
+  
   if(open) {
     return (
       <Grid container direction="row" style={{paddingTop: "8px", fontSize:"20px"}}>
-        <Grid item lg={12} xl={12} md={12} style={{maxHeight: '80vh',overflow:"auto", textAlign:"left", padding:"0px 40px 0px 0px"}}>
+        <Grid item lg={12} xl={12} md={12} style={{maxHeight: '80vh',overflow:"auto", textAlign:"left", padding:"0px 40px 0px 0px", ...overwriteStyle}}>
+            
             <Grid container>
               <Grid item>
                 <InfoIcon/>
@@ -21,6 +28,7 @@ export function TipsDialogContent({open, onClose} : TipsDialogContentProps) {
                 <h3 style={{display:"inline", paddingLeft:"5px"}}>Tip</h3>
               </Grid>
             </Grid>
+
             <h4>Get Started</h4>
             <p>To learn about a word or phrase, click on it.</p>
             <p>Once a word is selected it will be underlined in blue. The information about that word will appear in the right-hand panel</p>
