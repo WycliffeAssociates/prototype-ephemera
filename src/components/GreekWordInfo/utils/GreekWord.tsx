bde5b5fd48b5461e9225e595572d8a5a33c495d1
Darrell Cole Hill <darrellcolehill@gmail.com>
2023-05-19 14:29:12 -0400

    Update GWT information to use "Right Panel" settings

--- src/components/GreekWordInfo/utils/GreekWord.tsx
+++ src/components/GreekWordInfo/utils/GreekWord.tsx
@@ -1,6 +1,7 @@
 import Grid from '@mui/material/Grid';
 import { useSettings } from '../../../hooks/SettingsContext';
 import { mapValidGWTSettings } from './mapValidGWTSettings';
+import { useEffect, useState } from 'react';
 
 interface greekWordProps {
     greekWord: string | undefined,
@@ -8,13 +9,26 @@ interface greekWordProps {
 
 function GreekWord({greekWord} : greekWordProps) {
     const { GWTSettings } = useSettings();
+    const [fontSize, setFontSize] = useState("24px");
     let overwriteStyle : any = mapValidGWTSettings(GWTSettings);
+
+    useEffect(() => {
+        if(overwriteStyle?.fontSize != undefined) {
+            let nonAdjustedFontSize = parseInt(overwriteStyle.fontSize.match(/\d+/g));
+            let adjustedFontSize = 24 + (nonAdjustedFontSize - 20);
+    
+            let tempOverwriteStyle = {...overwriteStyle};
+            tempOverwriteStyle.fontSize = adjustedFontSize + "px";
+            setFontSize(adjustedFontSize + "px");
+        }
+    }, [GWTSettings]);
+
 
     return (
         <>
             <Grid item sm={12} xs={12}>
                 <p 
-                    style={{color:"blue"}}
+                    style={{color:"blue", ...overwriteStyle, fontSize:fontSize}}
                     className="GreekWord"
                 >{greekWord}</p> 
             </Grid>
