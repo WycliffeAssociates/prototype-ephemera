ec83cd6bc30ee938851caf570dc23d1842d6c08b
Darrell Cole Hill <darrellcolehill@gmail.com>
2023-05-19 16:17:29 -0400

    Update verse reference text to use "Right Panel" settings

--- src/components/VerseReferenceDialogContent.tsx
+++ src/components/VerseReferenceDialogContent.tsx
@@ -2,6 +2,8 @@ import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { VerseReferenceText } from './VerseReferenceText';
+import { useSettings } from '../hooks/SettingsContext';
+import { mapValidGWTSettings } from './GreekWordInfo/utils/mapValidGWTSettings';
 
 
 interface VerseReferenceDialogContentProps {
@@ -13,63 +15,80 @@ interface VerseReferenceDialogContentProps {
   
 
 export function VerseReferenceDialogContent({open, onClose, refBookChapterVerse, fullScreen} : VerseReferenceDialogContentProps) {
-    if(open) {
-      return (
-        <Grid container direction="row">
 
-          <Grid item lg={12} xl={12} md={12} sm={12} xs={12} style={{maxHeight:"fit-content", overflow:"hidden", textAlign:"left", paddingLeft:"15px"}}>
-              <Grid container>
+  const { GWTSettings } = useSettings();
+  let overwriteStyle : any = mapValidGWTSettings(GWTSettings);
+
+  if(open) {
+    return (
+      <Grid container direction="row">
+
+        <Grid 
+              item 
+              lg={12} xl={12} md={12} sm={12} xs={12} 
+              style={{
+                        maxHeight:"fit-content", 
+                        overflow:"hidden", 
+                        textAlign:"left", 
+                        paddingLeft:"15px"
+                     }}
+        >
+            <Grid container>
+
+              <Grid 
+                    item
+                    xl={8} lg={8} md={8} sm={8} xs={8} 
+                >
+                    <h3 
+                        style={{color: "#015AD9", float:"left", paddingLeft:"2%", ...overwriteStyle}}
+                    >
+                      {refBookChapterVerse.refBook} {refBookChapterVerse.refChapter}
+                    </h3>
+                </Grid>
 
                 <Grid 
-                      item
-                      xl={8} lg={8} md={8} sm={8} xs={8} 
-                  >
-                      <h3 style={{color: "#015AD9", float:"left", paddingLeft:"2%",}}>{refBookChapterVerse.refBook} {refBookChapterVerse.refChapter}</h3>
-                  </Grid>
-
-                  <Grid 
-                      item xl={2} lg={2} md={2} sm={2} xs={2} 
-                      style={{padding: "12px 0px 12px 16px", margin:"auto",}}
-                  >
-                      <Button onClick={onClose} variant="outlined" style={{
-                                                                            float:"right", 
-                                                                            border: "1px solid #E5E8EB", 
-                                                                            borderRadius: "16px", 
-                                                                            color:"#33445C", 
-                                                                            textTransform:"none",
-                                                                            minWidth: (fullScreen === false ? "115px" : "65px"),
-                                                                          }}
-                       >
-                          <ArrowBackIcon/> {fullScreen !== true ? "Go Back" : ""}
-                      </Button>
-                  </Grid> 
-
-                <Grid item 
-                      xl={12} lg={12} md={12} sm={12} xs={12} 
-                      id="verseReferenceContainer"
-                      style={{
-                              scrollMargin:"50px", 
-                              position: "relative", 
-                              maxHeight:(fullScreen ? "85vh" : "70vh"), 
-                              overflowY:"scroll",
-                              padding:"0px 40px 0px 0px",
-                      }}
+                    item xl={2} lg={2} md={2} sm={2} xs={2} 
+                    style={{padding: "12px 0px 12px 16px", margin:"auto",}}
                 >
-                  
-                  <VerseReferenceText 
-                    refBook={refBookChapterVerse.refBook} 
-                    refChapter={parseInt(refBookChapterVerse.refChapter)} 
-                    refVerse={refBookChapterVerse.refVerse} 
-                    refWord={refBookChapterVerse.refWord}
-                  />
-                </Grid>
+                    <Button onClick={onClose} variant="outlined" style={{
+                                                                          float:"right", 
+                                                                          border: "1px solid #E5E8EB", 
+                                                                          borderRadius: "16px", 
+                                                                          color:"#33445C", 
+                                                                          textTransform:"none",
+                                                                          minWidth: (fullScreen === false ? "115px" : "65px"),
+                                                                        }}
+                      >
+                        <ArrowBackIcon/> {fullScreen !== true ? "Go Back" : ""}
+                    </Button>
+                </Grid> 
+
+              <Grid item 
+                    xl={12} lg={12} md={12} sm={12} xs={12} 
+                    id="verseReferenceContainer"
+                    style={{
+                            scrollMargin:"50px", 
+                            position: "relative", 
+                            maxHeight:(fullScreen ? "85vh" : "70vh"), 
+                            overflowY:"scroll",
+                            padding:"0px 40px 0px 0px",
+                    }}
+              >
+                
+                <VerseReferenceText 
+                  refBook={refBookChapterVerse.refBook} 
+                  refChapter={parseInt(refBookChapterVerse.refChapter)} 
+                  refVerse={refBookChapterVerse.refVerse} 
+                  refWord={refBookChapterVerse.refWord}
+                />
               </Grid>
-          </Grid>
-      </Grid>
-      )
-    } else {
-      return <></>
-    }
+            </Grid>
+        </Grid>
+    </Grid>
+    )
+  } else {
+    return <></>
+  }
 
 
 }
--- src/components/VerseReferenceText.tsx
+++ src/components/VerseReferenceText.tsx
@@ -1,8 +1,8 @@
 import useChapterVerseData from "../hooks/useChapterVerseData";
-import { mapValidULBSettings } from './Text/utils/mapValidULBSettings';
 import { useSettings } from '../hooks/SettingsContext';
 import { useEffect, useRef, useState } from "react";
 import { NewFormattedGreekWord, NewFormattedWord, PhraseWord, SubWord } from "../types";
+import { mapValidGWTSettings } from "./GreekWordInfo/utils/mapValidGWTSettings";
 
 
 interface VerseReferenceTextProps {
@@ -15,12 +15,16 @@ interface VerseReferenceTextProps {
 export function VerseReferenceText({ refBook, refChapter, refVerse, refWord } : VerseReferenceTextProps ) {
 
     const verses = useChapterVerseData(refBook, refChapter);
-    const { ULBSettings } = useSettings();
     const [verseOutput, setVerseOutput] = useState<any[]>([]);
     const verseRef = useRef<HTMLSpanElement>(null);
+    
+    const { GWTSettings } = useSettings();
+    let overwriteStyle : any = mapValidGWTSettings(GWTSettings);
 
-    let wordOverwriteStyle : any = mapValidULBSettings(ULBSettings).wordStyles;
-    let verseOverwriteStyle : any = mapValidULBSettings(ULBSettings).verseStyles;
+    useEffect(() => {
+        console.log("changing settings");
+        console.log(overwriteStyle);
+    }, [GWTSettings])
 
     function checkSubWordsForReference(subWords : SubWord[] | undefined) {
         return subWords?.map((foo) => { 
@@ -50,21 +54,43 @@ export function VerseReferenceText({ refBook, refChapter, refVerse, refWord } : 
             verse.verseWords.forEach((verseWord, wordIdx) => {
                 if((verseIdx + 1) === parseInt(refVerse) && checkForReferences(verseWord)) {
                     verseWordOutput.push(<>
-                        <span ref={verseRef} className="TextContainer_GreekPhrase" style={{color:"#001533CC", textDecoration:"none", fontSize: wordOverwriteStyle.fontSize}}>
+                        <span 
+                            ref={verseRef} 
+                            className="TextContainer_GreekPhrase" 
+                            style={{
+                                    color:"#001533CC", 
+                                    textDecoration:"none", 
+                                    ...overwriteStyle                                
+                            }}
+                        >
                             <b>{verseWord.englishWords}</b>
                         </span>
                         <span> </span>
                     </>)
                 } else if(checkForReferences(verseWord)) {
                     verseWordOutput.push(<>
-                        <span className="TextContainer_GreekPhrase" style={{color:"#001533CC", textDecoration:"none", fontSize: wordOverwriteStyle.fontSize}}>
+                        <span 
+                            className="TextContainer_GreekPhrase" 
+                            style={{
+                                    color:"#001533CC", 
+                                    textDecoration:"none", 
+                                    ...overwriteStyle
+                                   }}
+                        >
                             <b>{verseWord.englishWords}</b>
                         </span>
                         <span> </span>
                     </>)
                 } else {
                     verseWordOutput.push(<>
-                        <span className="TextContainer_GreekPhrase" style={{color:"#001533CC", textDecoration:"none", fontSize: wordOverwriteStyle.fontSize}}>
+                        <span 
+                            className="TextContainer_GreekPhrase" 
+                            style={{
+                                        color:"#001533CC", 
+                                        textDecoration:"none", 
+                                        ...overwriteStyle
+                                   }}
+                        >
                             {verseWord.englishWords}
                         </span>
                         <span> </span>
@@ -75,13 +101,19 @@ export function VerseReferenceText({ refBook, refChapter, refVerse, refWord } : 
             })
     
             const tempVerse = (
-                <p id={`verse-${verse.verseNum}`}key={`verse + ${verse.verseNum}`} className="TextContainer__Verse" style={{...verseOverwriteStyle}}><sup>{verse.verseNum}</sup> {verseWordOutput}</p>
+                <p 
+                    id={`verse-${verse.verseNum}`} 
+                    key={`verse + ${verse.verseNum}`} 
+                    className="TextContainer__Verse" 
+                    style={{...overwriteStyle}}>
+                        <sup>{verse.verseNum}</sup> {verseWordOutput}
+                </p>
             )
             tempVerseOutput.push(tempVerse);
         });
 
         setVerseOutput([...tempVerseOutput])
-    }, [verses])
+    }, [verses, GWTSettings])
 
     
     useEffect(() => {
@@ -91,6 +123,7 @@ export function VerseReferenceText({ refBook, refChapter, refVerse, refWord } : 
             scrollableParent.scrollTo(0,verseRef.current.offsetTop - 40)
         }
     },[verseOutput])
+
 
 
     return (
