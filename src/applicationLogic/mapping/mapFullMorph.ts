76bc7504fe55015d1a9134e44f17cc985184d8ce
Darrell Cole Hill <darrellcolehill@gmail.com>
2023-06-09 11:57:52 -0400

    Proper links for first and second person

--- src/applicationLogic/mapping/mapFullMorph.ts
+++ src/applicationLogic/mapping/mapFullMorph.ts
@@ -3,6 +3,8 @@
 const fullMorphToFileName : any = {
     "person gentilic": "people group",
     "location gentilic": "people group",
+    "second": "second person",
+    "first": "first person"
 }
 
 
