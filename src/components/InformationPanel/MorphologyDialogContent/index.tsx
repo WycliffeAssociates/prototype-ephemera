abab8737ec29c82b81d406f8d762e5fd48e2a4fb
Darrell Hill <89662191+darrellcolehill@users.noreply.github.com>
2023-06-29 17:29:11 -0400

    Separated code for fetching morphology files

--- src/components/InformationPanel/MorphologyDialogContent/index.tsx
+++ src/components/InformationPanel/MorphologyDialogContent/index.tsx
@@ -6,43 +6,7 @@ import UnprocessedMarkdown from ".//utils/UnprocessedMarkdown";
 import { useSettings } from "../../../hooks/SettingsContext";
 import { mapValidGWTSettings } from "../GreekWordInfo/utils/mapValidGWTSettings";
 import { useGreekWords } from "../../../hooks/GreekWordsContext";
-import { fullMorphToFileName } from "../../../applicationLogic/mapping/mapFullMorph";
-
-interface missingReport {
-	word: string;
-	abbreviated: string;
-	full: string;
-	strongs: string;
-	ogntSort: string;
-}
-
-async function reportMissingMorphology(
-	data: missingReport
-) {
-	try {
-		let config = {
-			method: "post",
-			maxBodyLength: Infinity,
-			url: "https://wagwtfeedbackhandler.azurewebsites.net/api/MissingHandler",
-			headers: {
-				Accept: "*/*",
-				"User-Agent": "GWT",
-				"x-functions-key":
-					"LFRaycifocGWKg-97QV13FFYNxMpXop847cwTlxdgXb2AzFuQXNxbg==",
-				"Content-Type": "text/plain",
-			},
-			data: data,
-		};
-
-		let res = await axios.request(config);
-
-		console.log(
-			"report status = " + JSON.stringify(res.status)
-		);
-	} catch (error) {
-		console.log(error);
-	}
-}
+import { fetchMorphologyWord } from "../../../api/morphology"
 
 interface MorphologyDialogContentProps {
 	open: Boolean;
@@ -71,42 +35,12 @@ export default function MorphologyDialogContent({
 	}
 
 	useEffect(() => {
-		if (morphologyWord !== undefined) {
-			(async function () {
-				try {
-					let fileName: string = morphologyWord;
-
-					if (fullMorphToFileName[fileName] !== undefined) {
-						fileName = fullMorphToFileName[fileName];
-					}
-
-					let response = await axios.get(
-						`https://content.bibletranslationtools.org/WycliffeAssociates/en_gwt/raw/branch/master/02_morphology_files/${fileName}.md`
-					);
-					setMorphologyWordMarkdown(response.data);
-				} catch (error) {
-					if (greekWords) {
-						let greekWord = greekWords[
-							greekWords?.length - 1
-						] as any;
-						let requestBody = {
-							word: greekWords[greekWords?.length - 1].text,
-							abbreviated:
-								greekWords[greekWords?.length - 1].morph,
-							full: morphologyWord,
-							strongs:
-								greekWords[greekWords?.length - 1].strongs,
-							ogntSort:
-								(greekWord.OGNTsort
-									? greekWord.OGNTsort
-									: greekWord.OGNTSort) + "",
-						};
-
-						reportMissingMorphology(requestBody);
-					}
-				}
-			})();
-		}
+		(async () => {
+			let res = await fetchMorphologyWord(greekWords, morphologyWord);
+			if(res) {
+				setMorphologyWordMarkdown(res)
+			}
+		})();
 	}, []);
 
 	if (open) {
--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,9 @@ const DEFAULT_ULB_LINE_HEIGHT = 200;
 const DEFAULT_ULB_FONT_SIZE = 20;
 const DEFAULT_INFORMATIONPANEL_LINE_HEIGHT = 150;
 const DEFAULT_INFORMATIONPANEL_FONT_SIZE = 20;
-
+const MORPHOLOGY_FILE_PATH = "https://content.bibletranslationtools.org/WycliffeAssociates/en_gwt/raw/branch/master/02_morphology_files";
+const FEEDBACK_HANDLER_URL = "https://wagwtfeedbackhandler.azurewebsites.net/api/MissingHandler";
+const FEEDBACK_HANDLER_KEY = "LFRaycifocGWKg-97QV13FFYNxMpXop847cwTlxdgXb2AzFuQXNxbg==";
 
 export { 
     BIEL_LINK,
@@ -12,5 +14,8 @@ export {
     DEFAULT_ULB_LINE_HEIGHT,
     DEFAULT_ULB_FONT_SIZE,
     DEFAULT_INFORMATIONPANEL_LINE_HEIGHT,
-    DEFAULT_INFORMATIONPANEL_FONT_SIZE
+    DEFAULT_INFORMATIONPANEL_FONT_SIZE,
+    MORPHOLOGY_FILE_PATH,
+    FEEDBACK_HANDLER_URL,
+    FEEDBACK_HANDLER_KEY
 };
