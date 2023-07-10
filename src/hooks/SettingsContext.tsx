7c67851346cd1890f3160eefe820708758390c0d
Darrell Hill <89662191+darrellcolehill@users.noreply.github.com>
2023-07-10 17:03:09 -0400

    Saves/fetches settings using local storage

--- src/hooks/SettingsContext.tsx
+++ src/hooks/SettingsContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import {
 	SettingsOption,
 	ULBSettingsOption,
@@ -44,11 +44,13 @@ export function SettingsProvider({ children }: any) {
 			newValue:
 				| number
 				| string
-				| number
 				| boolean
 				| undefined
 		) {
 			setLeftPanelFontSizeValue(parseInt(newValue + ""));
+			if(newValue) {
+				writeSettingsToLocalStorage("ULB", "Font Size", newValue)
+			}
 		},
 		inputType: "increment",
 		unit: "px",
@@ -64,11 +66,13 @@ export function SettingsProvider({ children }: any) {
 			newValue:
 				| number
 				| string
-				| number
 				| boolean
 				| undefined
 		) {
 			setLeftPanelLineHeightValue(parseInt(newValue + ""));
+			if(newValue) {
+				writeSettingsToLocalStorage("ULB", "Line Height", newValue)
+			}
 		},
 		inputType: "increment",
 		unit: "%",
@@ -84,11 +88,13 @@ export function SettingsProvider({ children }: any) {
 			newValue:
 				| number
 				| string
-				| number
 				| boolean
 				| undefined
 		) {
 			setInformationPanelFontSizeValue(parseInt(newValue + ""));
+			if(newValue) {
+				writeSettingsToLocalStorage("GWT", "Font Size", newValue)
+			}
 		},
 		inputType: "increment",
 		unit: "px",
@@ -104,19 +110,91 @@ export function SettingsProvider({ children }: any) {
 				newValue:
 					| number
 					| string
-					| number
 					| boolean
 					| undefined
 			) {
 				setInformationPanelLineHeightValue(
 					parseInt(newValue + "")
 				);
+				if(newValue) {
+					writeSettingsToLocalStorage("GWT", "Line Height", newValue)
+				}
 			},
 			inputType: "increment",
 			unit: "%",
 			styleOverrideKey: "lineHeight",
 			defaultValue: DEFAULT_INFORMATIONPANEL_LINE_HEIGHT,
 		};
+
+	// TODO: write a function that takes in the settings options and value to write to local storage
+	type StoredSettings = {
+		[key: string]: {
+		  [key: string]: number | string | boolean;
+		};
+	  };
+
+	
+	function writeSettingsToLocalStorage(settingScope:string, settingName:string, settingValue:string | number | boolean) {
+		let storedSettings = localStorage.getItem("settings");
+
+		if (storedSettings) {
+			let previousSettings = JSON.parse(storedSettings);
+
+			previousSettings[settingScope][settingName] = settingValue;
+
+			localStorage.setItem(
+				"settings",
+				JSON.stringify(previousSettings)
+			);
+
+		} else {
+			let newSettings : StoredSettings = {"ULB":{}, "GWT":{}};
+
+			newSettings[settingScope][settingName] = settingValue;
+			
+
+			localStorage.setItem(
+				"settings",
+				JSON.stringify(newSettings)
+			);
+		}
+	}
+
+	function initializeStoredSettings() {
+		let initialSettings = {
+			"ULB" : {
+				"Font Size": leftPanelFontSizeValue,
+				"Line Height": leftPanelLineHeightValue
+			},
+			"GWT" : {
+				"Font Size": informationPanelFontSizeValue,
+				"Line Height": informationPanelLineHeightValue
+			}
+		}
+
+		localStorage.setItem(
+			"settings",
+			JSON.stringify(initialSettings)
+		);
+	}
+
+
+	useEffect(() => {
+
+		let storedSettings = localStorage.getItem("settings");
+		if(!storedSettings){
+			initializeStoredSettings();
+		} else {
+			let previousSettings = JSON.parse(storedSettings);
+
+			// TODO: refactor this when I get more feedback from Aby and Dian
+			setInformationPanelFontSizeValue(previousSettings["GWT"]["Font Size"]);
+			setInformationPanelLineHeightValue(previousSettings["GWT"]["Line Height"]);
+			setLeftPanelFontSizeValue(previousSettings["ULB"]["Font Size"]);
+			setLeftPanelLineHeightValue(previousSettings["ULB"]["Line Height"]);
+		}
+	}, [])
+
 
 	return (
 		<SettingsContext.Provider
