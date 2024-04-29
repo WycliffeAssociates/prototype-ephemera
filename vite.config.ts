fa8e68ad61edacd6e25bd321d564cf4663c157eb
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-04-29 14:06:30 -0400

    Removes process.env undefined errors from Proskomma package

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    // depending on your application, base can also be "/"
-    base: '',
-    plugins: [react(), viteTsconfigPaths()],
-    server: {    
-        // this ensures that the browser opens upon server start
-        open: true,
-        // this sets a default port to 3000  
-        port: 3000, 
-    },
-})
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        base: '',
+        plugins: [react(), viteTsconfigPaths()],
+        server: {    
+            open: true,
+            port: 3000, 
+        },
+        define: {
+            'process.env': env
+        },
+    };
+});
