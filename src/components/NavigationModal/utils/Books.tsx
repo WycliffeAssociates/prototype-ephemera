f2dd52d562b51a87b96d9e7b6656cac2d3b455f7
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-06-21 11:26:15 -0400

    uses !! instead of === true

--- src/components/NavigationModal/utils/Books.tsx
+++ src/components/NavigationModal/utils/Books.tsx
@@ -16,7 +16,7 @@ function Book({ bookData, handleClick, isCurrentBook }: BookProps) {
 	const bookRef = useRef<null | HTMLDivElement>(null);
 
 	useEffect(() => {
-		if (isCurrentBook === true) {
+		if (!!isCurrentBook) {
 			handleClick(bookRef);
 		}
 	}, [isCurrentBook]);
