eb6510b5a5ba9935bd80f472d31abad213818cba
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-07-12 11:05:41 -0400

    Removed unnecessary log

--- src/components/InformationWindow/utils/ContinueReadingButton.tsx
+++ src/components/InformationWindow/utils/ContinueReadingButton.tsx
@@ -12,7 +12,6 @@ export function ContinueReadingButton() {
 		>
 			<Grid item>
 				<Button
-					onClick={() => console.log("Figure out what Aby wants to do here")}
 					variant="text"
 					style={{
 						border: "1px solid #E5E8EB",
