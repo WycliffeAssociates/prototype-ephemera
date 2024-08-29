e068076a40fa736bb2ce8062b55e994048429ed1
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-08-29 15:45:39 -0400

    only uses AlignmentExplorationUsfmAccessor

--- src/api/SourceTextAccessor/index.ts
+++ src/api/SourceTextAccessor/index.ts
@@ -2,6 +2,8 @@ import type { AlignedVerse } from "../../types";
 import { Door43UsfmAccessor } from "./Door43UsfmAccessor";
 import { OsisEnUlbAccessor } from "./OsisEnUlbAccessor";
 import { WacsXmlAccessor } from "./WacsXmlAccessor";
+import { AlignmentExplorationUsfmAccessor } from "./AlignmentExplorationUsfmAccessor";
+
 
 export type SourceTextResource = {
 	resourceBaseURL: string;
@@ -28,16 +30,18 @@ export class SourceTextAccessorFactory {
 	private osisAccessor = new OsisEnUlbAccessor("ulb", "en");
 	private door43Accessor = new Door43UsfmAccessor("", "");
 	private wacsAccessor = new WacsXmlAccessor("ulb", "en");
+	private alignedExplorationUsfmAccessor = new AlignmentExplorationUsfmAccessor("ulb", "en")
 
 	getSourceTextAccessor(
 		resourceType: string,
 		resourceLanguage: string,
 	): SourceTextAccessor {
-		if (resourceType.toLocaleLowerCase() === "ulb") {
-			return this.wacsAccessor;
-		}
+		// if (resourceType.toLocaleLowerCase() === "ulb") {
+		// 	return this.wacsAccessor;
+		// }
 
-		this.door43Accessor.setSourceTextResource(resourceType, resourceLanguage);
-		return this.door43Accessor;
+		// this.door43Accessor.setSourceTextResource(resourceType, resourceLanguage);
+		// return this.door43Accessor;
+		return this.alignedExplorationUsfmAccessor;
 	}
 }
